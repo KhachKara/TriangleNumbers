a024923930865a32007be8e106c7d511e2c06549
Khachatur <khachkara@gmail.com>
2016-08-15 12:16:27 +0300

    с поправками Мгера

--- TriangleNumbers/TriangleNumbers/TriangleNumbers.cpp
+++ TriangleNumbers/TriangleNumbers/TriangleNumbers.cpp
@@ -1,7 +1,4 @@
-// TriangleNumbers.cpp: определяет точку входа для консольного приложения.
-//
-
-#include "stdafx.h"
+#include <stdafx.h>
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
@@ -12,28 +9,26 @@ using namespace std;
 
 int main()
 {
-	int StartNumber = 0, Input = 0, Output = 0;
+	unsigned long long int StartNumber = 0, Input = 0, Output = 0;
 	long double FinalNumber = 9223372036854775807;
 
 	cin >> Input;
-	if (Input > 0 && Input <= FinalNumber && Input) {
-		for ( int i = 0; i <= Input; i++)
+	if (Input > 0 && Input <= FinalNumber) {
+		for (int i = 0; i <= Input; i++)
 		{
 			StartNumber = StartNumber + i;
-				if (StartNumber == Input) {
-					Output = i;
-					cout << "input: " << Input << endl << "Output: " << Output;
-						break;
-				}
+			if (StartNumber == Input) {
+				Output = i;
+				cout << Output;
+				break;
+			}
 		}
-		if (Output) {
-
-		}
-		else (cout << "0");
-	} else (cout << "0");
-
+		if (!Output)
+			cout << 0;
+	}
+	else
+		cout << 0;
 	cin.get();
 	cin.get();
 	return 0;
-
 }
